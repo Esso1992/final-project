ca49a3e939c9586924a1285f053460a67401d390
Essam Mohamed Abd Elhamied <engessamabdelhamied@outlook.com>
2024-01-20 00:38:17 +0200

    Test2

--- service/urls.py
+++ service/urls.py
@@ -1,12 +1,15 @@
 # service/urls.py
-from django.urls import path
-from .routes import list_all_products, list_by_name, list_by_category, list_by_availability
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
-    # Your other URLs
-    path('api/products/all/', list_all_products, name='list_all_products'),
-    path('api/products/name/<str:name>/', list_by_name, name='list_by_name'),
-    path('api/products/category/<str:category>/', list_by_category, name='list_by_category'),
-    path('api/products/availability/<str:availability>/', list_by_availability, name='list_by_availability'),
+    path('api/', include(router.urls)),
+    path('api/products/all/', ProductViewSet.as_view({'get': 'list_all_products'}), name='list_all_products'),
+    path('api/products/name/<str:name>/', ProductViewSet.as_view({'get': 'list_by_name'}), name='list_by_name'),
+    path('api/products/category/<str:category>/', ProductViewSet.as_view({'get': 'list_by_category'}), name='list_by_category'),
+    path('api/products/availability/<str:availability>/', ProductViewSet.as_view({'get': 'list_by_availability'}), name='list_by_availability'),
 ]
-
